0a3788bcdd692f7e4506825d093b0c6cdc6419f0
debasishsahoo <debasish.sahoo96@gmail.com>
2022-02-11 10:47:16 +0530

    Promise All

--- BLOG-PROBLEM/promiseAll.js
+++ BLOG-PROBLEM/promiseAll.js
@@ -1,1 +1,10 @@
-const promise1 =Promise.res
+const promise1 = Promise.resolve('debasish');
+
+const promise2 = 10;
+
+const promise3 = new Promise((resolve, reject) => {
+    setTimeout(resolve, 2000, 'Good Morning');
+})
+
+const promise4 = fetch('http://fakestoreapi.com/products')
+    .then(res => res.json());
