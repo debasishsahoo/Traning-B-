2aaa61fe0acbc18618644d0ee18a88204c26426b
debasishsahoo <debasish.sahoo96@gmail.com>
2022-02-11 10:27:04 +0530

    Promise

--- BLOG-PROBLEM/infoProblem.js
+++ BLOG-PROBLEM/infoProblem.js
@@ -1,46 +1,64 @@
-function GetFullName(fn, ln) {
-    setTimeout(function () {
-        return fn + " " + ln;
-    }, 2000)
+function GetFullName(fn) {
+    return new Promise((resolve, reject) => {
+        setTimeout(function () {
+            if (fn !== '') {
+                resolve(fn);
+            } else {
+                reject('Please Provide a Full Name');
+            }
+        }, 2000)
+    });
+
+
 }
 function GetHobbies(name) {
-    let hobbies = ['Cricket', 'Vollyball', 'Badminton', 'coding'];
-    setTimeout(function () {
-        myHobby = []
-        if (name !== '') {
-            if (name == 'deb') {
-                myHobby = hobbies[3]
+    return new Promise((resolve, reject) => {
+        let hobbies = ['Cricket', 'Vollyball', 'Badminton', 'coding'];
+        setTimeout(function () {
+            let myHobby = '';
+            if (name !== '') {
+                if (name == 'deb') {
+                    myHobby = hobbies[3]
+                }
+                else if (name == 'mir') {
+                    myHobby = hobbies[2]
+                }
+                else if (name == 'abir') {
+                    myHobby = hobbies[0]
+                }
+                else if (name == 'poly') {
+                    myHobby = hobbies[1]
+                }
+                else {
+                    myHobby = hobbies[0]
+                }
+                resolve(myHobby)
+            } else {
+                reject('Please Provide Name');
             }
-            else if (name == 'mir') {
-                myHobby = hobbies[2]
-            }
-            else if (name == 'abir') {
-                myHobby = hobbies[0]
-            }
-            else if (name == 'poly') {
-                myHobby = hobbies[1]
-            }
-            else {
-                myHobby = hobbies[0]
-            }
-        } else {
-            console.log('Please Provide Name');
-        }
 
-    }, 4000)
+        }, 4000)
+    });
+
 }
 function GetHobbyDetails(hobby) {
-    setTimeout(function () {
-        if (hobby !== '') {
-            let details = {
-                'Cricket': 'i love Cricket',
-                'Vollyball': 'i love Vollyball',
-                'Badminton': 'i love Badminton',
-                'coding': 'i love coding'
+    return new Promise((resolve, reject) => {
+        setTimeout(function () {
+            if (hobby !== '') {
+                let details = {
+                    'Cricket': 'i love Cricket',
+                    'Vollyball': 'i love Vollyball',
+                    'Badminton': 'i love Badminton',
+                    'coding': 'i love coding'
+                }
+                resolve(details[hobby])
+            } else {
+                reject('Please Provide Hoobby');
             }
-            return details[hobby]
-        } else {
-            console.log('Please Provide Hoobby');
-        }
-    }, 6000);
+        }, 6000);
+    })
+
 }
+
+
+
