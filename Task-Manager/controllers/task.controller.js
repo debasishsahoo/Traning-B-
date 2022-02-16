a02cc63fe1a45a9af9939813fa11ee78dd050baa
debasishsahoo <debasish.sahoo96@gmail.com>
2022-02-16 12:33:32 +0530

    APi

--- Task-Manager/controllers/task.controller.js
+++ Task-Manager/controllers/task.controller.js
@@ -1,10 +1,90 @@
+const Task = require('../models/task.model')
 
-const getAllTask = () => { }
-const createTask = () => { }
-const getTask = () => { }
-const updatePutTask = () => { }
-const updatePatchTask = () => { }
-const deleteTask = () => { }
+const getAllTask = async (req, res) => {
+    try {
+        const task = await Task.find({})
+
+        if (!task || task.length <= 0) { return res.status(404).json({ success: false, message: 'Task not found' }) }
+
+        res.status(200).json({ success: true, message: 'Task Found', task: task })
+    }
+    catch (error) {
+        res.status(500).json({ success: false, message: error })
+    }
+}
+const createTask = async (req, res) => {
+    const { name, iscomplete } = req.body
+    try {
+        const task = await Task.create({ name: name, iscomplete: iscomplete });
+
+        if (!task) { return res.status(404).json({ success: false, message: 'Task Not Inserted' }) }
+
+        res.status(201).json({ success: true, message: `Task Inserted with id ${task._id} `, task: task })
+    }
+    catch (error) {
+        res.status(500).json({ success: false, message: error })
+    }
+}
+const getTask = async (req, res) => {
+    try {
+        const { id: taskId } = req.params
+        const task = await Task.findOne({ _id: taskId })
+
+        if (!task) { return res.status(404).json({ success: false, message: 'Task not found' }) }
+
+        res.status(200).json({ success: true, message: 'Task Found', task: task })
+    }
+    catch (error) {
+        res.status(500).json({ success: false, message: error })
+    }
+}
+const updatePutTask = async (req, res) => {
+    try {
+        const { id: taskId } = req.params
+        const task = await Task.findOneAndUpdate({ _id: taskId }, req.body, {
+            new: true,
+            runValidators: true,
+            overwrite: true
+        })
+
+        if (!task) { return res.status(404).json({ success: false, message: `Task not found with ${taskId}` }) }
+
+        res.status(200).json({ success: true, message: 'Task Updated', task: task })
+    }
+    catch (error) {
+        res.status(500).json({ success: false, message: error })
+    }
+}
+
+const updatePatchTask = async (req, res) => {
+    try {
+        const { id: taskId } = req.params
+        const task = await Task.findOneAndUpdate({ _id: taskId }, req.body, {
+            new: true,
+            runValidators: true
+        })
+
+        if (!task) { return res.status(404).json({ success: false, message: `Task not found with ${taskId}` }) }
+
+        res.status(200).json({ success: true, message: 'Task Updated', task: task })
+    }
+    catch (error) {
+        res.status(500).json({ success: false, message: error })
+    }
+}
+const deleteTask = async (req, res) => {
+    try {
+        const { id: taskId } = req.params
+        const task = await Task.findOneAndDelete({ _id: taskId })
+
+        if (!task) { return res.status(404).json({ success: false, message: `Task not found with ${taskId}` }) }
+
+        res.status(200).json({ success: true, message: 'Task Deleted', task: null })
+    }
+    catch (error) {
+        res.status(500).json({ success: false, message: error })
+    }
+}
 
 
 module.exports = {
--- Task-Manager/models/task.model.js
+++ Task-Manager/models/task.model.js
@@ -3,14 +3,14 @@ const mongoose = require('mongoose')
 const TaskSchema = new mongoose.Schema({
     name: {
         type: String,
-        required: true,
+        required: [true, 'must provide name'],
         trim: true,
         minlength: [3, 'Name can not be less then 3 Characters'],
         maxlength: [20, 'Name can not be more then 20 Characters']
     },
     iscomplete: {
         type: Boolean,
-        default:false,
+        //default: false,
     }
 })
 module.exports = mongoose.model('Task', TaskSchema)    
