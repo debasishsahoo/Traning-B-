693bc8e960333a47312f9231e549ce69c302312f
debasishsahoo <debasish.sahoo96@gmail.com>
2022-02-18 22:35:06 +0530

    Numirac

--- Store-api/controllers/product.controller.js
+++ Store-api/controllers/product.controller.js
@@ -1,4 +1,4 @@
-const Product = require('../models/product.model')
+const Product = require('../models/product.model');
 
 const getAllTestProduct = async (req, res) => {
     //const Products = await Product.find({}).limit(10);
@@ -6,92 +6,97 @@ const getAllTestProduct = async (req, res) => {
     const Products = await Product.find({}).sort('price -name');
 
     if (Products.length === 0) {
-        return res.status(404).json({ success: false, message: 'No Product Found' })
+        return res
+            .status(404)
+            .json({ success: false, message: 'No Product Found' });
     }
     res.status(200).json({
         requestQuery: req.query,
         success: true,
         count: Products.length,
-        Products: Products
-    })
-}
-
-
-
+        Products: Products,
+    });
+};
 
 const getAllDynamicsProduct = async (req, res) => {
-
     //DB BLOCK
-    const { featured, name, company, sort, fields } = req.query
-    const QueryObject = {}
+    const { featured, name, company, sort, fields, numericFilter } = req.query;
+    const QueryObject = {};
     if (featured) {
-        QueryObject.featured = featured === 'true' ? true : false
+        QueryObject.featured = featured === 'true' ? true : false;
     }
     if (name) {
-        QueryObject.name = { $regex: name, $options: 'i' }
+        QueryObject.name = { $regex: name, $options: 'i' };
     }
     if (company) {
-        QueryObject.company = { $regex: company, $options: 'i' }
+        QueryObject.company = { $regex: company, $options: 'i' };
+    }
+    if (numericFilter) {
+        const operatorMap = {
+            '>': '$gt',
+            '>=': '$gte',
+            '=': '$eq',
+            '<': '$lt',
+            '<=': '$lte'
+        };
+        const regEx = /\b(<|>|>=|=<|<=)\b/g;
+        let filters = numericFilter.replace(
+            regEx,
+            (match) => `-${operatorMap[match]}-`
+        );
+
+        const options = ['price', 'rating']
+        filters = filters.split(',').forEach((item) => {
+            const [field, operator, value] = item.split('-');
+            if (options.includes(field)) {
+                QueryObject[field] = { [operator]: Number(value) };
+            }
+        })
+
+
+
     }
 
     //const Products = await Product.find(QueryObject);
-    let result = Product.find(QueryObject); //1200000000
+    let result = Product.find(QueryObject); //12k
 
-    //SORT  
+    //SORT
     if (sort) {
-        const sortList = sort.split(',').join(' ')
-        result = result.sort(sortList)
+        const sortList = sort.split(',').join(' ');
+        result = result.sort(sortList);
     } else {
-        result = result.sort('-createdAt')
+        result = result.sort('-createdAt');
     }
 
     if (fields) {
-        const fieldsList = fields.split(',').join(' ')
-        result = result.select(fieldsList)
+        const fieldsList = fields.split(',').join(' ');
+        result = result.select(fieldsList);
     }
-
-
-
-
-
-
 
     //In memory Block
     const page = Number(req.query.page) || 1;
     const limit = Number(req.query.limit) || 100;
     const skip = Number(page - 1) * limit;
 
-    result = result.limit(limit).skip(skip)
+    result = result.limit(limit).skip(skip);
 
     const Products = await result;
 
-
-
-
-    //https://www.flipkart.com/search?q=mobile&otracker=search&otracker1=search&marketplace=FLIPKART&as-show=on&as=off&page=31
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     if (Products.length === 0) {
-        return res.status(404).json({ QueryObject: QueryObject, success: false, message: 'No Product Found' })
+        return res
+            .status(404)
+            .json({
+                QueryObject: QueryObject,
+                success: false,
+                message: 'No Product Found',
+            });
     }
     res.status(200).json({
         requestQuery: req.query,
         QueryObject: QueryObject,
         success: true,
         count: Products.length,
-        Products: Products
-    })
-}
-module.exports = { getAllTestProduct, getAllDynamicsProduct }
+        Products: Products,
+    });
+};
+module.exports = { getAllTestProduct, getAllDynamicsProduct };
