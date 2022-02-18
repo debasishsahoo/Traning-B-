ef120b1ddaa9ba90685939b043028b6c56c892bd
debasishsahoo <debasish.sahoo96@gmail.com>
2022-02-18 21:59:15 +0530

    Field

--- Store-api/controllers/product.controller.js
+++ Store-api/controllers/product.controller.js
@@ -2,7 +2,8 @@ const Product = require('../models/product.model')
 
 const getAllTestProduct = async (req, res) => {
     //const Products = await Product.find({}).limit(10);
-    const Products = await Product.find({}).skip(20);
+
+    const Products = await Product.find({}).sort('price -name');
 
     if (Products.length === 0) {
         return res.status(404).json({ success: false, message: 'No Product Found' })
@@ -21,7 +22,7 @@ const getAllTestProduct = async (req, res) => {
 const getAllDynamicsProduct = async (req, res) => {
 
     //DB BLOCK
-    const { featured, name, company } = req.query
+    const { featured, name, company, sort, fields } = req.query
     const QueryObject = {}
     if (featured) {
         QueryObject.featured = featured === 'true' ? true : false
@@ -36,10 +37,28 @@ const getAllDynamicsProduct = async (req, res) => {
     //const Products = await Product.find(QueryObject);
     let result = Product.find(QueryObject); //1200000000
 
+    //SORT  
+    if (sort) {
+        const sortList = sort.split(',').join(' ')
+        result = result.sort(sortList)
+    } else {
+        result = result.sort('-createdAt')
+    }
+
+    if (fields) {
+        const fieldsList = fields.split(',').join(' ')
+        result = result.select(fieldsList)
+    }
+
+
+
+
+
+
 
     //In memory Block
     const page = Number(req.query.page) || 1;
-    const limit = Number(req.query.limit) || 10;
+    const limit = Number(req.query.limit) || 100;
     const skip = Number(page - 1) * limit;
 
     result = result.limit(limit).skip(skip)
