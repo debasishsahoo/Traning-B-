b9eea4bab17571523cafe540107384514cdd3c34
debasishsahoo <debasish.sahoo96@gmail.com>
2022-03-01 12:30:10 +0530

    Swagger Doc Code

--- BANK-API/models/customer.model.js
+++ BANK-API/models/customer.model.js
@@ -1,53 +1,96 @@
-const mongoose = require("mongoose");
-const Joi = require("joi");
+const mongoose = require('mongoose');
+const Joi = require('joi');
 
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const autoIdGenerator = require('../middlewares/autoIdGenerator');
 
-const checkEmail = require("../middlewares/uniqueEmail");
-const checkPhone = require("../middlewares/uniquePhone");
+const checkEmail = require('../middlewares/uniqueEmail');
+const checkPhone = require('../middlewares/uniquePhone');
 
+/**
+ * @openapi
+ * components:
+ *  schemas:
+ *      UserInput:
+ *          type: object
+ *          required:
+ *              - full_name
+ *              - email
+ *              - contact_no
+ *              - password
+ *          properties:
+ *              full_name:
+ *                  type: string
+ *                  default: First-Middle-Last
+ *              email:
+ *                  type: string
+ *                  default: example@company.domain
+ *              contact_no:
+ *                  type: number
+ *                  default: 123456790
+ *              password:
+ *                  type: string
+ *                  default: Example@10
+ *      UserResponse:
+ *          type: object
+ *          properties:
+ *              _id:
+ *                  type: string
+ *              full_name:
+ *                  type: string
+ *              email:
+ *                  type: string
+ *              contact_no:
+ *                  type: number
+ *              password:
+ *                  type: string
+ *              CIF_No:
+ *                  type: string
+ *              status:
+ *                  type: string
+ *              createdAt:
+ *                  type: string
+ */
 
 const CustomerSchema = new mongoose.Schema(
   {
-    name: Joi.string().pattern(new RegExp("^[a-zA-Z]{3,30}$")),
-    email: Joi.string().required().trim().lowercase().external(checkEmail).email(),
+    name: Joi.string().pattern(new RegExp('^[a-zA-Z]{3,30}$')),
+    email: Joi.string()
+      .required()
+      .trim()
+      .lowercase()
+      .external(checkEmail)
+      .email(),
     contactNo: Joi.number().max(10).min(10).external(checkPhone),
     password: Joi.string().required(),
     CIF_No: Joi.string().alphanum(),
-    isActive: Joi.string().valid("active", "deactive", "blocked", "notVarified").default("notVarified"),
+    isActive: Joi.string()
+      .valid('active', 'deactive', 'blocked', 'notVarified')
+      .default('notVarified'),
     createdAt: Joi.date().default(Date.now()),
   },
   { timestamps: true }
-)
+);
 CustomerSchema.pre('create', async function () {
-
-  const salt = await bcrypt.genSalt(Number(process.env.SECRET_KEY))
-  this.password = await bcrypt.hash(this.password, salt)
+  const salt = await bcrypt.genSalt(Number(process.env.SECRET_KEY));
+  this.password = await bcrypt.hash(this.password, salt);
   this.CIF_No = await autoIdGenerator();
-
-})
+});
 CustomerSchema.pre('findByIdAndUpdate', async function () {
-
-  const salt = await bcrypt.genSalt(Number(process.env.SECRET_KEY))
-  this.password = await bcrypt.hash(this.password, salt)
-
-
-})
+  const salt = await bcrypt.genSalt(Number(process.env.SECRET_KEY));
+  this.password = await bcrypt.hash(this.password, salt);
+});
 
 CustomerSchema.methods.createJWT = async function () {
-  return jwt.sign(
-    { userId: this._id, name: this.name },
-    process.env.EncKey,
-    { expiresIn: process.env.JWT_LIFETIME }
-  )
-
-}
+  return jwt.sign({ userId: this._id, name: this.name }, process.env.EncKey, {
+    expiresIn: process.env.JWT_LIFETIME,
+  });
+};
 
 CustomerSchema.methods.comparePassword = async function (canditatePassword) {
-  const isMatch = await bcrypt.compare(canditatePassword, this.password)
-  return isMatch
-}
+  const isMatch = await bcrypt.compare(canditatePassword, this.password);
+  return isMatch;
+};
 
-module.exports = mongoose.model("Customer", CustomerSchema);
+module.exports = mongoose.model('Customer', CustomerSchema);
--- BANK-API/routes/customer.router.js
+++ BANK-API/routes/customer.router.js
@@ -12,15 +12,191 @@ const {
   CustomerPasswordChange
 } = require("../controllers/customer.controller");
 
-// 1. Bank Customer
-// 2. Bank Employee
-
-// Customer Can sign up, sign in, create new Account, Edit Account, Delete Account,
-// Customer Can Deposit, withdrawal money from account, they can receive and send money from another user
-
-// Employee login, check balance of a particular account and block a particular account they give authorization to a Transaction.
-
-// without bank employee authorizing no Transaction can happened
+/**
+ * @openapi
+ * '/api/v1/user/':
+ *      get:
+ *              tags:
+ *              - User
+ *              summary: Get all user's data
+ *              responses:
+ *                      200:
+ *                              description: Success
+ *                              content:
+ *                                      application/json:
+ *                                              schema:
+ *                                                      $ref: '#/components/schemas/UserResponse'
+ *                      404:
+ *                              description: Users not found
+ * 
+ * '/api/v1/user/signup':
+ *      post:
+ *              tags:
+ *              - User
+ *              summary: Register an User
+ *              requestBody:
+ *                      required: true
+ *                      content:
+ *                              application/json:
+ *                                      schema:
+ *                                              $ref: '#/components/schemas/UserInput'
+ *              responses:
+ *                      201:
+ *                              description: Created
+ *                              content:
+ *                                      application/json:
+ *                                              schema:
+ *                                                      $ref: '#/components/schemas/UserResponse'
+ *                      400:
+ *                              description: Bad Request
+ * 
+ * '/api/v1/user/signin':
+ *      post:
+ *              tags:
+ *              - User
+ *              summary: Login an User
+ *              requestBody:
+ *                      required: true
+ *                      content:
+ *                              application/json:
+ *                                      schema:
+ *                                              type: object
+ *                                              required:
+ *                                                      - email
+ *                                                      - password
+ *                                              properties:
+ *                                                      email:
+ *                                                              type: string
+ *                                                              default: example@company.domain
+ *                                                      password:
+ *                                                              type: string
+ *                                                              default: Example@10
+ *              responses:
+ *                      202:
+ *                              description: Accepted
+ *                              content:
+ *                                      application/json:
+ *                                              schema:
+ *                                                      type: object
+ *                      400:
+ *                              description: Bad Request
+ * 
+ * '/api/v1/user/{id}':
+ *      get:
+ *              tags:
+ *              - User
+ *              summary: Get an user by id
+ *              parameters:
+ *                      - name: id
+ *                        in: path
+ *                        description: The user of id
+ *                        required: true
+ *              response:
+ *                      200: 
+ *                              description: Success
+ *                              content:
+ *                                      application/json:
+ *                                              schema:
+ *                                                      $ref: '#/components/schemas/UserResponse'
+ *                      404:
+ *                              description: Not Found
+ *      patch:
+ *              tags:
+ *              - User
+ *              summary: Update an user by id
+ *              parameters:
+ *                      - name: id
+ *                        in: path
+ *                        description: The user of id
+ *                        required: true
+ *              requestBody:
+ *                      required: true
+ *                      content:
+ *                              application/json:
+ *                                      schema:
+ *                                              type: object
+ *                                              required:
+ *                                                      - name
+ *                                                      - contact_no
+ *                                              properties:
+ *                                                      name:
+ *                                                              type: string
+ *                                                              default: First-Middle-Last
+ *                                                      contact_no:
+ *                                                              type: number
+ *                                                              default: 1234567890
+ *              responses:
+ *                      202:
+ *                              description: Accepted
+ *                              content:
+ *                                      application/json:
+ *                                              schema:
+ *                                                      $ref: '#/components/schemas/UserResponse'
+ *                      400:
+ *                              description: Bad Request
+ * 
+ * '/api/v1/user/{id}/cif/{cif}':
+ *      delete:
+ *              tags:
+ *              - User
+ *              summary: Deactivate an user by id
+ *              parameters:
+ *                      - name: id
+ *                        in: path
+ *                        description: The user of id
+ *                        required: true
+ *                      - name: cif
+ *                        in: path
+ *                        description: Customer Identification Number
+ *                        required: true
+ *              responses:
+ *                      202:
+ *                              description: Accepted
+ *                              content:
+ *                                      application/json:
+ *                                              schema:
+ *                                                      $ref: '#/components/schemas/UserResponse'
+ *                      400:
+ *                              description: Bad Request
+ *      patch:
+ *              tags:
+ *              - User
+ *              summary: Change password of an user by id
+ *              parameters:
+ *                      - name: id
+ *                        in: path
+ *                        description: The user of id
+ *                        required: true
+ *                      - name: cif
+ *                        in: path
+ *                        description: Customer Identification Number
+ *                        required: true
+ *              requestBody:
+ *                      required: true
+ *                      content:
+ *                              application/json:
+ *                                      schema:
+ *                                              type: object
+ *                                              required:
+ *                                                      - email
+ *                                                      - password
+ *                                              properties:
+ *                                                      email:
+ *                                                              type: string
+ *                                                              default: First-Middle-Last
+ *                                                      password:
+ *                                                              type: string
+ *                                                              default: Example@10
+ *              responses:
+ *                      202:
+ *                              description: Accepted
+ *                              content:
+ *                                      application/json:
+ *                                              schema:
+ *                                                      $ref: '#/components/schemas/UserResponse'
+ *                      400:
+ *                              description: Bad Request
+ */
 
 router.route("/").get(GetAllCustomer);
 
